9c50b28a9118f1cb7357a84622933e4f136fc622
Brent Acs <brent.mann@gmail.com>
2021-02-19 14:08:05 -0500

    EoL 15.211 added a custom validator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,12 @@ import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 export class AppComponent implements OnInit {
   genders = ["male", "female"];
   signupForm: FormGroup;
+  forbiddenUserNames = ['Theresa', 'Darren'];
 
   ngOnInit() {
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
-      'username': new FormControl(null, Validators.required),
+      'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
       'email': new FormControl(null, [Validators.required, Validators.email]),
       }),
       'gender': new FormControl("male"),
@@ -27,7 +28,6 @@ export class AppComponent implements OnInit {
 
   onAddHobby(){
     const control = new FormControl(null, Validators.required);
-    //const control = new FormControl(null);
     (<FormArray>this.signupForm.get('hobbies')).push(control);
   }
 
@@ -35,5 +35,12 @@ export class AppComponent implements OnInit {
     const controls = (<FormArray>this.signupForm.get("hobbies")).controls;
     console.log('controls length: ' + controls.length );
     return controls;
+  }
+
+  forbiddenNames(control: FormControl): {[s: string]: boolean} {
+    if(this.forbiddenUserNames.indexOf(control.value) !== -1){
+      return {'nameisForbidden': true};
+    }
+    return null;
   }
 }
