ef7ee1ac260d375713f81d4b3db33ba8f4a87497
Brent Acs <brent.mann@gmail.com>
2021-02-19 12:41:51 -0500

    EoL 15.205 - wired up HTML to FormGroup object and implemented onSubmit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,20 +2,24 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
-  genders = ['male', 'female'];
+  genders = ["male", "female"];
   signupForm: FormGroup;
 
-  ngOnInit(){
+  ngOnInit() {
     this.signupForm = new FormGroup({
-      'username': new FormControl(null),
-      'email': new FormControl(null),
-      'gender': new FormControl('male'),
-     });
+      username: new FormControl(null),
+      email: new FormControl(null),
+      gender: new FormControl("male"),
+    });
   }
 
+  onSubmit(){
+    console.log(this.signupForm);
+    
+  }
 }
